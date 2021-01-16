ba67e8143ff7f42ec57c866569f4e225a04ccfd8
Ted de Koning <ted@q42.nl>
2021-01-16 22:23:45 +0100

    Client - Adding some style for navigation (work in progress).

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,70 +1,28 @@
-import logo from './logo.svg';
 import './App.scss';
-import {SearchCarClient} from "./proto/searchCar_pb_service";
-import {AnnualCostsRequest, Car, SearchRequest} from "./proto/searchCar_pb";
-import {useState} from "react";
-import CarList from "./components/CarList";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import {BrowserRouter as Router, Link, Route, Switch, NavLink} from "react-router-dom";
 import CarCollection from "./screens/CarCollection";
 import AddCar from "./screens/AddCar";
 import AnnualCosts from "./screens/AnnualCosts";
 
-const client = new SearchCarClient('http://localhost:8080')
+import './components/TopNavigation.scss'
 
 function App() {
 
-  const [cars, setCars] = useState<Car[] | undefined>(undefined)
-
-  async function doStuff () {
-    const searchRequest = new SearchRequest()
-    searchRequest.setManufacturer("Opel")
-    await client.search(searchRequest, (error, responseMessage) => {
-      setCars(responseMessage?.getCarsList())
-      // const carList = responseMessage?.getCarsList()
-      // if(carList && carList.length > 0) {
-      //   console.log('response cars!', carList[0].getManufacturer(), carList[0].getReleaseyear(), carList.length)
-      // } else {
-      //   console.log('No cars found')
-      // }
-      // if(error) {
-      //   console.error('ERRR', error)
-      // }
-    })
-  }
-
   return (
     <div className="App">
-      {/*<header className="App-header">*/}
-
-      {/*  <button onClick={doStuff}>*/}
-      {/*    Search cars*/}
-      {/*  </button>*/}
-
-      {/*  <button onClick={addCar}>Add car</button>*/}
-
-      {/*  <button onClick={getAnnualCostsList}>Get annual costs per car</button>*/}
-
-      {/*  {cars && <CarList cars={cars}/>}*/}
-
-      {/*</header>*/}
       <Router>
         <div>
-          <nav>
+          <nav className="top-navigation">
             <ul>
               <li>
-                <Link to="/">Catalogue</Link>
+                <NavLink activeClassName="active" exact={true} to="/">Catalogue</NavLink>
               </li>
               <li>
-                <Link to="/add-car">Add car</Link>
+                <NavLink activeClassName="active" exact={true} to="/add-car">Add car</NavLink>
               </li>
               <li>
-                <Link to="/annual-costs">Annual costs</Link>
+                <NavLink activeClassName="active" exact={true} to="/annual-costs">Annual costs</NavLink>
               </li>
             </ul>
           </nav>
